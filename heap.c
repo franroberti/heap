298143f6361acb314029e7359f08b42e2c9991c9
Mariano <mariano@live.com>
2016-10-26 01:34:42 -0300

    Posibles errores comentados

--- heap.c
+++ heap.c
@@ -1,6 +1,9 @@
+#include "heap.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-#define CANTIDAD_INICIAL 20
-
+#define TAM_INICIAL 20
 //redim
 #define FACTOR_DE_CARGA 0.7
 #define INCREMENTO_CAPACIDAD 2
@@ -8,12 +11,13 @@
 
 struct heap{
 
-	char *array[];
+	//char *array[];
+	void* array[];//No se si esto funciona, por ahi con void* es suficiente, pero char* no
 	size_t cantidad;
 	size_t capacidad;
 	cmp_func_t cmp;
 
-};f
+};
 
 heap_t *heap_crear(cmp_func_t cmp){
 	
@@ -22,7 +26,8 @@ heap_t *heap_crear(cmp_func_t cmp){
 	if(!heap)
 		return NULL;
 
-	heap->array = malloc(sizeof(char*)*CANTIDAD_INICIAL);	
+	//heap->array = malloc(sizeof(char*)*CANTIDAD_INICIAL);
+	heap -> array = malloc(sizeof(void*)*TAM_INICIAL);
 
 	if(!heap->array){
 		free(heap);
@@ -30,11 +35,13 @@ heap_t *heap_crear(cmp_func_t cmp){
 	}
 
 	heap->cantidad = 0;
-	heap->capacidad = CANTIDAD_INICIAL;
+	heap->capacidad = TAM_INICIAL;
 	heap->cmp = cmp;
 
 	return heap;
 }
+
+
 
 heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
 	
@@ -42,12 +49,14 @@ heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
 	
 	for(size_t i=0;<n;i++){	
 
-		if(heap->cantidad >= (heap->capacidad*FACTOR_DE_CARGA))
+		if(heap->cantidad >= (heap->capacidad*FACTOR_DE_CARGA))//Podemos meter esto en la funcion redimensionar,
+			//llamarla siempre para no repetir codigo y que ella decida si agrandar, achicar o no hacer nada
+			//(Correcion de Martin)
 			heap_redimensionar(heap,heap->capacidad*INCREMENTO_CAPACIDAD); //podria validar
 
-		heap->arreglo[i] = arreglo[i];
-
-		heap->cantidad ++;	
+		//heap->arreglo[i] = arreglo[i];// Si hacemos esto el heap queda con el mismo orden que el arreglo
+		//heap->cantidad ++;	
+		heap_encolar(heap, arreglo[i]);
 	}
 		
 	return heap;
@@ -73,20 +82,19 @@ void heap_destruir(heap_t *heap, void destruir_elemento(void *e)){
 	for(size_t i=0;i<heap->cantidad;i++){
 		if(destruir_elemento)
 			destruir_elemento(heap->arreglo[i]);	
-
-		free(heap->arreglo);
 	}
-	
+	free(heap->arreglo);
 	free(heap);
 }
 
 bool heap_redimensionar(heap_t *heap,size_t nueva_dim){
 	void *aux_array;
-
+	//Te olvidaste de poner un heap_aux
 	if(!heap_aux)
 		return false;
 
-	aux = realloc(heap->arreglo,sizeof(char*)*nueva_dim);
+
+	aux = realloc(heap->arreglo,sizeof(void*)*nueva_dim);
 
 	if(!aux){
 		heap_destruir(heap_aux);
@@ -108,7 +116,9 @@ bool heap_encolar(heap_t *heap,void *elemento){
 	
 	heap->arreglo[cantidad] = elemento;
 
-	heap_sort(heap);//reorganizar el arreglo
+	//heap_sort(heap);//reorganizar el arreglo
+
+	//Hacer upheap porque el unico elemento que por ahi esta desordenado es el del final
 
 	heap->cantidad ++;
 	
@@ -126,7 +136,9 @@ void *heap_desencolar(heap){
 	
 	heap->arreglo[0] = heap->arreglo[cantidad-1];	
 	
-	heap_sort(heap);//reorganizar
+	//heap_sort(heap);//reorganizar
+
+	//Hacer downheap(heap_sort no porque se supone que hay un solo elemento desordenado, y es el primero)
 
 	heap->cantidad --;
 
@@ -140,26 +152,28 @@ void heapify(heap_t *heap){
 	}
 }
 
-//podrian recivir el arreglo down y up heap, y heapify lo mismo
+//podrian recibir el arreglo down y up heap, y heapify lo mismo
 void downheap(heap_t *heap,size_t posicion){
-	size_t pos_h_izq,posi_h_der,pos_mayor;
+	size_t pos_h_izq,i,pos_h_der,pos_mayor;
 	
 	if(posicion > heap->cantidad)
 		return;
 
 	pos_h_izq = posicion*2+1;
 	pos_h_der = posicion*2+2;
-	pos_mayor = i;
+	pos_mayor = i; //i no se usa nunca 
 
 	if(pos_h_izq < heap->cantidad && heap->arreglo[posicion] < heap->arreglo[pos_h_izq])
 		pos_mayor = pos_h_izq;
 
 	if(pos_h_der < heap->cantidad && heap->arreglo[pos_mayor] < heap->arreglo[pos_h_der])
-		pos_mayor - pos_h_der;
+		pos_mayor = pos_h_der;
 
 	if(pos_mayor != posicion)
-		swap(heap->arreglo[posicion],heap->arreglo[pos_mayor])
+		swap(heap->arreglo[posicion],heap->arreglo[pos_mayor]);
 
+	//No lo vi con suficiente tiempo, pero creo que puede pasar que la posicion sea la mayor de todas y en 
+	//este caso pos_mayor no va a estar definida.(En nel caso de que quede un solo elemento por ejemplo)
 	downheap(heap,pos_mayor);
 }
 
